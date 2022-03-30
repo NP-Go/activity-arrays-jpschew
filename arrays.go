ef6aafd1a2160a6141dfa9afd0fff588bb1fc3b2
jpschew <jps.chew@gmail.com>
2022-03-30 22:50:13 +0800

    added code to get length of array

--- arrays.go
+++ arrays.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert Code here
+	var data [7]string
+	data[0] = "Operating System List"
+	data[1] = "Windows XP"
+	data[2] = "Linux 1.0"
+	data[3] = "Raspbian Teensy"
+	data[4] = "MacOS"
+	data[5] = "IOS"
+	data[6] = "Google Android"
+
+	fmt.Println(len(data))
 }
